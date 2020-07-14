cdad8423fd968117ce8a7ec5fed86fa8b4461a64
Maximilian Jendrall <hello@max.berlin>
2020-07-14 18:38:06 +0200

    Add: cors function

--- turven-server.js
+++ turven-server.js
@@ -7,6 +7,7 @@ var cors = require('cors');
 app.enable('trust proxy');
 
 // enable cors
+app.use(cors())
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
